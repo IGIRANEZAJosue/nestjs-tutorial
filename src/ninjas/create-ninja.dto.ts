8600f01353e27240de16e37c8edbfe705033f1bc
Josue Igiraneza <igiranezaj28@gmail.com>
2024-10-07 17:33:52 +0200

    add validation for the weapon type

--- src/ninjas/create-ninja.dto.ts
+++ src/ninjas/create-ninja.dto.ts
@@ -1,8 +1,11 @@
-import { MinLength } from 'class-validator';
+import { IsEnum, MinLength } from 'class-validator';
 
 export class CreateNinjaDto {
   @MinLength(3)
   name: string;
 
+  @IsEnum(['stars', 'nunchucks'], {
+    message: 'Weapon should either be star or nunchucks',
+  })
   weapon: 'stars' | 'nunchucks';
 }
